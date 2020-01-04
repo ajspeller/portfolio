6e030c7ebf7b2837b7ec9bf6921d67eacf257344
AJ Speller <ajspeller@gmail.com>
2020-01-03 23:13:42 -0500

    Menu button, javascript and animation

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// set initial state
+let showMenu = false;
+
+const toggleMenu = () => {
+  if (showMenu) {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => {
+      item.classList.remove('show');
+    });
+  } else {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => {
+      item.classList.add('show');
+    });
+  }
+  showMenu = !showMenu;
+};
+
+menuBtn.addEventListener('click', toggleMenu);
